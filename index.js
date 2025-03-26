b8a8b60dd773f2d11d19cd85100d6f0afa64654e
Kizito Mrema <kizomanizo@gmail.com>
2025-03-26 11:14:24 +0300

    MINOR: Bug fixes and refactoring

--- index.js
+++ index.js
@@ -42,31 +42,15 @@ class AppServer {
     this.app.disable("x-powered-by");
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(
-      cors({
-        origin: function (origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true);
-          }
-          return callback(new Error("Not allowed by CORS"));
-        },
-        credentials: true,
-      })
-    );
+    this.app.use(cors());
     this.app.use(SecurityMiddleware.applyHelmet());
     this.app.set("views", path.join(__dirname, "views"));
     this.app.set("view engine", "pug");
     this.app.use(express.static(path.join(__dirname, "public")));
-    this.app.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          defaultSrc: ["'self'"],
-          scriptSrc: ["'self'"],
-          styleSrc: ["'self'"],
-          connectSrc: ["'self'", "http://170.187.199.69:3035"],
-          imgSrc: ["'self'", "data:"],
-          objectSrc: ["'none'"],
-        },
+    app.use(
+      helmet({
+        crossOriginOpenerPolicy: false,
+        originAgentCluster: false,
       })
     );
   }
