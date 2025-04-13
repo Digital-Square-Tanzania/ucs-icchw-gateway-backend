41063beec17054582d764caa18d55122a1f09da2
Kizito Mrema <kizomanizo@gmail.com>
2025-04-13 21:51:59 +0300

    MINOR: Bug fixes and refactoring

--- modules/user/user-service.js
+++ modules/user/user-service.js
@@ -77,9 +77,9 @@ class UserService {
         where: { userUuid: activation.userUuid },
       });
 
-      if (!member) return { alert: true, message: "Kiungo ulichotumia sio sahihi.", slug, login: false };
+      if (!member) return { alert: true, message: "Kiungo ulichotumia sio sahihi, member render.", slug, login: false };
 
-      if (!activation) return { alert: true, message: "Kiungo ulichotumia sio sahihi.", slug, login: false };
+      if (!activation) return { alert: true, message: "Kiungo ulichotumia sio sahihi, activation render.", slug, login: false };
       if (activation.isUsed) return { alert: true, message: "Akaunti hii tayari inatumika.", slug, login: false };
       if (Date.now() > activation.expiryDate) return { alert: true, message: "Muda wa kuwasha akaunti umepitiliza.", slug, login: false, resend: true, username: member.username };
       console.log("🔄 Valid activation link received, rendering activation page for slug: ", slug);
@@ -108,7 +108,7 @@ class UserService {
       where: { userUuid: activation.userUuid },
     });
 
-    if (!member) return { alert: true, message: "Kiungo ulichotumia sio sahihi.", slug, login: false };
+    if (!member) return { alert: true, message: "Kiungo ulichotumia sio sahihi, activate no member.", slug, login: false };
     if (password !== confirmPassword) {
       return { alert: true, message: "Password ulizoingiza hazifanani.", slug, login: true, username: member.username };
     }
