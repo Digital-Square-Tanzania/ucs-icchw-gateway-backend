06c7180d78b6993a7f30f7ff86a65b60742e6a3c
Kizito Mrema <kizomanizo@gmail.com>
2025-04-16 09:05:21 +0300

    MINOR: Bug fixes and refactoring

--- utils/openmrs-api-client.js
+++ utils/openmrs-api-client.js
@@ -35,7 +35,7 @@ class OpenMRSApiClient {
       return response.data;
     } catch (error) {
       // console.error(`❌ OpenMRS POST Error (${endpoint}):`, error.response?.data || error.message);
-      console.error(`❌ OpenMRS POST Error (${endpoint}): ${error.message}`, JSON.stringify(response.data, null, 2));
+      console.error(`❌ OpenMRS POST Error (${endpoint}): `, error.message);
       return error;
     }
   }
