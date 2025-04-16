864d73c6f640da8e4edea80df70aaa2d13d33765
Kizito Mrema <kizomanizo@gmail.com>
2025-04-16 12:39:45 +0300

    MINOR: Bug fixes and refactoring

--- utils/postgres-client.js
+++ utils/postgres-client.js
@@ -9,11 +9,12 @@ class PostgresClient {
     this.pool = new Pool({
       // connectionString: process.env.OPENSRP_DB_URL,
       host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT, 10),
-      database: process.env.PG_DATABASE,
-      user: process.env.PG_USER,
+      port: parseInt(process.env.PG_PORT, 10) || 5432,
+      database: process.env.PG_DATABASE || "opensrp",
+      user: process.env.PG_USER || "postgres",
       password: process.env.PG_PASSWORD,
       ssl: process.env.PG_SSL === "false",
+      schema: process.env.PG_SCHEMA || "public",
     });
   }
 
