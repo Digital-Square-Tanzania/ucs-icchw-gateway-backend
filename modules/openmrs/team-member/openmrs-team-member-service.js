5b33002e1e260169237f39900798216803b03353
Kizito Mrema <kizomanizo@gmail.com>
2025-06-19 13:14:24 +0300

    TEST: Rejecting existing usernames - 7

--- modules/openmrs/team-member/openmrs-team-member-service.js
+++ modules/openmrs/team-member/openmrs-team-member-service.js
@@ -379,18 +379,28 @@ class TeamMemberService {
         }
 
         // Check if the CHW exists in team members by NIN
-        const identifiedTeamMember = await TeamMemberRepository.getTeamMemberByIdentifier(cleaned.identifier);
-
-        if (identifiedTeamMember) {
-          // throw new CustomError("Duplicate CHW ID found.", 409, 2);
+        const teamMemberExists = await mysqlClient.query("SELECT uuid, identifier FROM users WHERE identifier = ?", [row.identifier.trim()]);
+        if (teamMemberExists.length > 0) {
           rejected.push({
             ...row,
-            rejectionReason: "Duplicate team member already exists",
+            rejectionReason: "✔ Team Member is already registered.",
             rowNumber: index + 2,
           });
-          console.warn(`🚨 Duplicate CHW ID found: ${cleaned.identifier}, process aborted...`);
           continue;
         }
+
+        // const identifiedTeamMember = await TeamMemberRepository.getTeamMemberByIdentifier(cleaned.identifier);
+
+        // if (identifiedTeamMember) {
+        //   // throw new CustomError("Duplicate CHW ID found.", 409, 2);
+        //   rejected.push({
+        //     ...row,
+        //     rejectionReason: "Duplicate team member already exists",
+        //     rowNumber: index + 2,
+        //   });
+        //   console.warn(`🚨 Duplicate CHW ID found: ${cleaned.identifier}, process aborted...`);
+        //   continue;
+        // }
 
         const teamRoleUuid = process.env.UCS_PROD_PROVIDER_ROLE_UUID_PROD;
         const teamMemberObject = {
